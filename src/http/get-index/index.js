3b40890d580a5c3e1ad6a14ce06fe05128680e64
ShawnHosea <shawnhosea@shawnhosea.com>
2021-03-09 00:43:17 -0500

    working

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,21 +1,46 @@
 const arc = require('@architect/functions')
-let tiny = require('tiny-json-http')
-// let stxCard = require('@architect/views/stxCard')
+const tiny = require('tiny-json-http')
+const { TwitterClient } = require('twitter-api-client')
 
 
 exports.handler = async function http (req) {
 
   let logo = arc.static('logo.png')
 
+  const twitterClient = new TwitterClient({
+    apiKey: process.env.TWITTER_API_KEY,
+    apiSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
+
   let url = `https://api.coincap.io/v2/assets/blockstack`
   let result = await tiny.get({url})
   let stx = result.body.data
-  console.log(stx)
-  
-  // let userCard = stxCard(coins)
 
-  // let cards = coins.map(coin => stxCard(coin)).join( " ")
-  // console.log(cards)
+  
+ const status = await tiny.get({url})
+    .then(response => {
+      let stx = response.body.data
+      let tweet
+      if (stx) {
+        //tweet the $STX price
+        tweet = 'Daily $STX price: ' + "\n" + "\n" + '$' + stx.priceUsd
+      } 
+      return tweet
+    }).catch(err => {
+      console.error(err)
+    })
+
+    console.log('Log2:', status)
+
+    await twitterClient.tweets.statusesUpdate({
+      status: status
+    }).then(response => {
+      console.log("Tweeted!", response)
+    }).catch(err => {
+      console.error(err)
+    })
 
   return {
     statusCode: 200,
--- src/scheduled/daily/index.js
+++ src/scheduled/daily/index.js
@@ -1,4 +1,4 @@
-const arc = require('@architect/functions')
+// const arc = require('@architect/functions')
 const tiny = require('tiny-json-http')
 const { TwitterClient } = require('twitter-api-client')
 
@@ -13,14 +13,33 @@ exports.handler = async function scheduled (event) {
   })
 
   let url = `https://api.coincap.io/v2/assets/blockstack`
-  let result = await tiny.get({url})
-  let stx = result.body.data
-  console.log(stx)
+  // let result = await tiny.get({url})
+  // let stx = result.body.data
+  // console.log(stx)
  
 
-  let tweet = stx.priceUsd
+  const status = await tiny.get({url})
+  .then(response => {
+    let stx = response.body.data
+    let tweet
+    if (stx) {
+      //tweet the $STX price
+      tweet = 'Daily $STX price: ' + "\n" + "\n" + '$' + stx.priceUsd
+    } 
+    return tweet
+  }).catch(err => {
+    console.error(err)
+  })
 
+  console.log('Log2:', status)
 
+  await twitterClient.tweets.statusesUpdate({
+    status: status
+  }).then(response => {
+    console.log("Tweeted!", response)
+  }).catch(err => {
+    console.error(err)
+  })
 	
   console.log(JSON.stringify(event, null, 2))
   return
